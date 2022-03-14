9ecb6cca231f84232ac72166d480bd73cddc5eac
cr0zyy <zyoxpus@hotmail.com>
2022-03-14 10:31:55 +0100

    Fixed slope bouncing

--- Insane Game/Assets/scripts/PlayerMovement.cs
+++ Insane Game/Assets/scripts/PlayerMovement.cs
@@ -7,23 +7,24 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
-    public float gravity = -19.62f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     public float jumpHeight = 2f;
-
+    public float gravity = -19.62f;
+    
     Vector3 velocity;
     bool isGrounded;
 
     void Update()
-    {
+    {   
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if(isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = -20f;
         }
         
         //fixes glitches when jumping close to small walls
@@ -44,14 +45,15 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
-
-        }
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
     }
 
 }
